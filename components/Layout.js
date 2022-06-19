7e4a04bf0830167514edffb88f62b01ff7619e53
Farhan <xpalmas@gmail.com>
2022-06-19 18:48:21 +0600

    styleClass

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,24 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
-import Head from 'next/head'
-import React, { Children } from 'react'
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import Head from 'next/head';
+import React, { Children } from 'react';
+import useStyles from '../utils/styles';
 
 export default function Layout({children}) {
+  const classes = useStyles();  
   return (
     <div>
         <Head>
             <title>Glossy Ecom</title>
         </Head>
-        <AppBar position='static'>
+        <AppBar position='static' className={classes.navbar}>
             <Toolbar>
                 <Typography>Glossy</Typography>
             </Toolbar>
         </AppBar>
-        <Container>
+        <Container className={classes.main}>
             {children}
         </Container>
-        <footer>
+        <footer className={classes.footer}>
             <Typography>
               All rights reserved. Next Glossy.  
             </Typography>
